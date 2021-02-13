fd024b5f7275b8ca6e2243c0323d7f9651fde487
EmisonLu <lzh1633856298@gmail.com>
2021-02-13 23:03:13 +0800

    完成上传数据的衔接(不考虑前端加密的情况)

--- netdisk/controllers/contactus.go
+++ netdisk/controllers/contactus.go
@@ -10,5 +10,7 @@ type ContactusController struct {
 }
 
 func (c *ContactusController) Show(){
+	// do_query(aes_encrypt("3 the"))
+	// search_title(aes_encrypt("1 Sky"))
 	c.TplName = "contactus.html"
 }
--- netdisk/controllers/upload.go
+++ netdisk/controllers/upload.go
@@ -1,11 +1,15 @@
 package controllers
 
 import (
+	"bufio"
+	"io"
 	"log"
+	"fmt"
 	"netdisk/models"
 	"strings"
 
 	"github.com/beego/beego/v2/client/orm"
+	"strconv"
 )
 
 func (c *MainController) ShowUpload() {
@@ -35,6 +39,45 @@ func (c *MainController) Upload() {
 		c.Ctx.WriteString("文件已存在, 请删除后重试")
 		return
 	}
+	// ========================将文件内容存储至SGX中======================
+
+	// 读取文件内容
+    reader := bufio.NewReader(file)
+	Text := ""
+    for {
+        str, err := reader.ReadString('\n') //读到一个换行就结束
+		Text = Text + str
+        if err == io.EOF {                  //io.EOF 表示文件的末尾
+            break
+        }
+    }
+	fmt.Println(Text)
+
+	// 读取当前用户ID
+	o := orm.NewOrm()
+	user := models.User{Name: c.UserName()}
+	err = o.Read(&user, "Name")
+	if err != nil {
+		c.Redirect("/login", 302)
+		return
+	}
+	userId := strconv.Itoa(user.Id)
+	fmt.Println(userId)
+
+	// 获取文章标题，并转换成userId + ' ' + Title的形式
+	filenameId := filename[0 : length-5]
+	filenameId = userId + " " + filenameId
+	fmt.Println(filenameId)
+
+	// 组织RawInput
+	file_update := RawInput{
+		Id: filenameId,
+		User: userId,
+		Text: Text,
+	}
+
+	build_index_and_commit(aes_encrypt(json_to_string(file_update)))
+	// ========================end of 将文件内容存储至SGX中======================
 
 	err = c.SaveToFile("file", "fileStorage/"+c.UserName()+"/"+filename)
 	log.Println(err)

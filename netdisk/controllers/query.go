17079e8a1ee918af8911787d83322e0fb2da0337
EmisonLu <lzh1633856298@gmail.com>
2021-03-29 14:18:50 +0800

    Fuzzy query front-end down

--- netdisk/controllers/query.go
+++ netdisk/controllers/query.go
@@ -18,10 +18,17 @@ func (c *QueryController) QueryGet() {
 	ReturnData := make(map[string]interface{})
 
 	package_str := c.GetString("package_str")
+	isfuzzy := c.GetString("isfuzzy")
+
+	if(isfuzzy == "1"){
+		ReturnData["message"] = do_query(package_str, 1)
+	} else {
+		ReturnData["message"] = do_query(package_str, 0)
+	}
 	// fmt.Println(package_str)
 
 	ReturnData["res"] = "1"
-	ReturnData["message"] = do_query(package_str)
+
 
 	c.Data["json"] = ReturnData
 	c.ServeJSON()
